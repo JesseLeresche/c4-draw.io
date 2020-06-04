802afa51cf4c50fa2f8a46ab549b98287672d1b1
Jesse Leresche <Jesse.Leresche@gmail.com>
2020-06-04 22:40:01 +0200

    Created user_story.js

---
+++ user_story.js
@@ -0,0 +1,15 @@
+
+/**
+ * Draw.io Plugin to create C4 Architecture Diagramms
+ *
+ * LOAD C4 SHAPE LIBRARY:
+ *
+ * https://raw.githubusercontent.com/tobiashochguertel/draw-io/master/C4-drawIO.xml
+ */
+Draw.loadPlugin(function (ui) {
+    var sidebar_id = 'userStory';
+    var sidebar_title = 'User Story';
+
+    
+    ///// END <- CUSTOM EDITOR FORM
+});
